d9df513b94c04328f4a37bd8ffdc4c2c90189078
Rohan Agarwal <98341888+RohanAgarwal26@users.noreply.github.com>
2023-03-29 21:41:52 +0530

    revised commit

---
+++ BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ericg.sudofiemed;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.rohan.healthcare";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "Health Care V1.0.1";
+}
